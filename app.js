15cf1278f859ba27d4d0bf6c42166c2e6c94c3fe
AtsushiHasebe021268 <hasebe.suisho.212@icloud.com>
2024-07-30 20:04:53 +0900

    fix app.js

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); // ここを確認
 
 app.use('/', index);
 app.use('/users', users);
